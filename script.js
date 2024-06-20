3f4059ad8091899736d01a030b2f07df449ff57a
rgargade <123531250+rgargade@users.noreply.github.com>
2024-06-20 17:35:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function showTime(){
   const currentTime = new Date();
   const time = `${currentTime.getHours()}: ${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
   document.getElementById('time').innerText = time;
-  console.log(time);
 }
 
 let interval = setInterval(showTime,1000);
